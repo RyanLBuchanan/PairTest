5899004923fbb86b146f781926c46bdeb9e37324
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-10-10 14:33:33 -0600

    feat: Working from https://zetcode.com/csharp/generics/

--- PairTest/PairTest.cs
+++ PairTest/PairTest.cs
@@ -2,6 +2,7 @@
 {
     internal class PairTest
     {
+
         static void Main(string[] args)
         {
             var firstElement = new Pair<string>();
@@ -31,10 +32,47 @@
 
             public override string ToString()
             {
-                return F.ToString();
+                //return F.ToString();
                 return S.ToString();
             }
         }
+
+
+
+
+        //static void Main(string[] args)
+        //{
+        //    var firstElement = new Pair<string>();
+        //    firstElement.F = "string";
+        //    Console.WriteLine(firstElement);
+
+        //    var secondElement = new Pair<int>();
+        //    secondElement.S = 1;
+        //    Console.WriteLine(secondElement);
+
+        //    //var ds3 = new Pair<bool>();
+        //    //ds3.Data = false;
+        //    //Console.WriteLine(ds3);
+
+
+
+
+
+        //    //Pair<F, S> pair = new Pair<F, S>("string", 1);
+
+        //}
+
+        //public class Pair<T>
+        //{
+        //    public string F { get; set; }
+        //    public int S { get; set; }
+
+        //    public override string ToString()
+        //    {
+        //        //return F.ToString();
+        //        return S.ToString();
+        //    }
+        //}
 
 
 
